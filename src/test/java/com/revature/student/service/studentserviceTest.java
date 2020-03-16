32f581fd9dac711ca33db2ab72f7a84fbf42d21b
tech-freak404 <bala98subramaian@gmail.com>
2020-03-16 08:09:05 -0400

    Test Cases

---
+++ src/test/java/com/revature/student/service/studentserviceTest.java
@@ -0,0 +1,44 @@
+package com.revature.student.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class studentserviceTest {
+
+	@Test
+	void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetstudbyInst() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetstudbyInstYear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+}
